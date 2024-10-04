0073ca651e63e2791d6f8036f18c28b7753f6226
Rahul Kapoor <138798654+logiclaws@users.noreply.github.com>
2024-10-04 17:01:00 +0000

    worked conversion and operations

---
+++ 01_Basics/03_ConversionOperations.js
@@ -0,0 +1,82 @@
+// let score = "33"
+// let score = "str"
+// let score = null
+// let score = undefined
+// let score = true
+
+// console.log(typeof score)
+// console.log(typeof(score)) // another method
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// if passes string and conversion will get NaN.
+// console.log(valueInNumber);
+
+
+// number can be easily converted
+// "abc" string will give value NaN - Not a number
+// true will convert to 1, false to 0
+
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+// let isLoggedIn = "Rahul"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true, 0 => false
+// "" => false, "some value" => true
+
+
+// let someNumber = 33;
+
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+// *************** Operations *************** //
+
+let value = 3
+
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " rahul"
+
+// console.log(str1 + str2);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+// console.table([num1,num2,num3])
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
